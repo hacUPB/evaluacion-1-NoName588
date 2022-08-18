a5553986153e31ebd2ae4ef35976e5fcc071948a
Davidkraai23 <>
2022-08-17 20:17:19 -0500

    c
    m

--- dirTest/project/test-wcat.sh
+++ dirTest/project/test-wcat.sh
@@ -1,36 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#! /bin/bash
 
- 
+if ! [[ -x wcat ]]; then
+    echo "wcat executable does not exist"
+    exit 1
+fi
 
-int main(int argc, char *argv[]){
-
- 
-
-     for(int i=1;i<argc;i++)
-        {
-        FILE *fin = fopen(argv[i],"r");
-
- 
-
-        if(fin == NULL){
-            //perror("fopen-fin fails: ");
-            printf("wcat: cannot open file\n");
-            exit(EXIT_FAILURE);
-        }
-
- 
-
-        char buffer[64];
-        char *status = NULL;
-        do{
-            status = fgets(buffer, sizeof(buffer),fin);
-            if(status != NULL){
-                printf("%s",buffer);
-            }
-        }while(status !=NULL);
-        
-        fclose(fin);
-        }
-    exit(EXIT_SUCCESS);
-}
+../../tester/run-tests.sh -v $*
